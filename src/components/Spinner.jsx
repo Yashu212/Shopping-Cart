9bd7261913ca295bf7b4130ee75a53eee9e0f838
Yashu212 <yashuklaaa212@gmail.com>
2024-08-10 21:27:15 +0530

    Updates

--- src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "./Spinner.css"
-const Spinner = () => {
-  return (
-    <span class="loader"></span>
-  )
-}
-
-export default Spinner
--- src/redux/Store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import cartSlice from './Slices/cartSlice'
-
-export const store = configureStore({
-  reducer: {cart:cartSlice},
-})
